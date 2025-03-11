d83fb4600031488645cbbc74d5f00058a255a1aa
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-03-11 17:13:22 +0100

    Update

---
+++ Test/SnipeITWebApiUnitTestShare/SnipeITBaseUnitTest.cs
@@ -0,0 +1,7 @@
+﻿namespace SnipeITWebApiUnitTest;
+
+public abstract class SnipeITBaseUnitTest
+{
+    protected const string storeKey = "artifactory-trial";
+    protected const string appName = "UnitTest";
+}
---
+++ Test/SnipeITWebApiUnitTestShare/SnipeITMainUnitTest.cs
@@ -0,0 +1,35 @@
+﻿namespace SnipeITWebApiUnitTest;
+
+[TestClass]
+public class SnipeITMainUnitTest : SnipeITBaseUnitTest
+{
+
+    [TestMethod]
+    public async Task TestMethodGetHardwareListAsync()
+    {
+        using var snipeIT = new SnipeIT(storeKey, appName);
+
+        var asyncList = snipeIT.GetHardwareListAsync();
+
+        var list = await asyncList.ToListAsync();
+
+        Assert.IsNotNull(list);
+        Assert.IsNotEmpty(list);
+        
+
+    }
+
+    [TestMethod]
+    public async Task TestMethodGetHardwareListByCategoryAsync()
+    {
+        using var snipeIT = new SnipeIT(storeKey, appName);
+
+        var asyncList = snipeIT.GetHardwareListByCategoryAsync(2);
+
+        var list = await asyncList.ToListAsync();
+
+        Assert.IsNotNull(list);
+        Assert.IsNotEmpty(list);
+
+    }
+}
