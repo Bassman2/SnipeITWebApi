dc69b3988df505b87106ed1cdf6c0fec6999ff3b
Ralf Beckers <ralf.beckers@hotmail.com>
2025-03-23 21:29:28 +0100

    Update

--- Src/SnipeITWebApiShare/Service/Model/UserModel.cs
+++ Src/SnipeITWebApiShare/Service/Model/UserModel.cs
@@ -20,6 +20,50 @@ internal class UserModel
     [JsonPropertyName("username")]
     public string? Username { get; set; }
 
+    [JsonPropertyName("remote")]
+    public bool Remote { get; set; }
+
+    [JsonPropertyName("locale")]
+    public string? Locale { get; set; }
+
+    [JsonPropertyName("employee_num")]
+    public string? EmployeeNum { get; set; }
+
+    [JsonPropertyName("manager")]
+    public NamedItemModel? Manager { get; set; }
+
+    [JsonPropertyName("jobtitle")]
+    public string? Jobtitle { get; set; }
+
+    [JsonPropertyName("vip")]
+    public bool Vip { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string? Phone { get; set; }
+
+    [JsonPropertyName("website")]
+    public string? Website { get; set; }
+
+    [JsonPropertyName("address")]
+    public string? Address { get; set; }
+
+    [JsonPropertyName("city")]
+    public string? City { get; set; }
+
+    [JsonPropertyName("state")]
+    public string? State { get; set; }
+
+    [JsonPropertyName("country")]
+    public string? Country { get; set; }
+
+    [JsonPropertyName("zip")]
+    public string? Zip { get; set; }
+
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
+
+    [JsonPropertyName("department")]
+    public NamedItemModel? Department { get; set; }
 
     // TODO ...
 
