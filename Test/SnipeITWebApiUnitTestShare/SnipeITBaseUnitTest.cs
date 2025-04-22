befa61352941d855c408cfab60d0a3ab58db5e37
Ralf Beckers <ralf.beckers@hotmail.com>
2025-04-22 15:27:07 +0200

    Update

--- Test/SnipeITWebApiUnitTestShare/SnipeITBaseUnitTest.cs
+++ Test/SnipeITWebApiUnitTestShare/SnipeITBaseUnitTest.cs
@@ -135,21 +135,21 @@ public abstract class SnipeITBaseUnitTest
 
     protected const int modelCreateId = 1;
     protected const string modelCreateName = "Macbook Pro 13&quot;";
-    protected const string modelCreateNumber = "4532230160393106";
+    protected const string modelCreateNumber = "4532501983915840";
     protected const int modelCreateCategoryId = 1;
     protected const string modelCreateCategoryName = "Laptops";
     protected readonly NamedItem? modelCreateManufacturer = null;
 
     protected const int modelUpdateId = 2;
     protected const string modelUpdateName = "Macbook Air";
-    protected const string modelUpdateNumber = "3528835919718805";
+    protected const string modelUpdateNumber = "4532646425353462";
     protected const int modelUpdateCategoryId = 1;
     protected const string modelUpdateCategoryName = "Laptops";
-    protected readonly NamedItem? modelUpdateManufacturer = (1, "example");
+    protected readonly NamedItem? modelUpdateManufacturer = (1, "Apple");
 
     protected const int modelPatchId = 4;
     protected const string modelPatchName = "XPS 13";
-    protected const string modelPatchNumber = "4164654283655320";
+    protected const string modelPatchNumber = "4532087105639";
     protected const int modelPatchCategoryId = 1;
     protected const string modelPatchCategoryName = "Laptops";
     protected readonly NamedItem? modelPatchManufacturer = (3, "Dell");
